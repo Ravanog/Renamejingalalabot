80f589d4c3808d3c88c67fcffcc6c639501acf40
Ravanog <149065101+Ravanog@users.noreply.github.com>
2024-12-06 11:54:56 +0530

    Update metadata.py

--- plugins/metadata.py
+++ plugins/metadata.py
@@ -1,8 +1,9 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
-from helper.database import db
+from helper.database import *
 from pyromod.exceptions import ListenerTimeout
-from config import Txt
+from config import *
+
 
 
 ON = [[InlineKeyboardButton('Metadata On ✅', callback_data='metadata_1')], [
@@ -15,14 +16,12 @@ OFF = [[InlineKeyboardButton('Metadata Off ❌', callback_data='metadata_0')], [
 async def handle_metadata(bot: Client, message: Message):
 
     ms = await message.reply_text("**Please Wait...**", reply_to_message_id=message.id)
-    bool_metadata = await db.get_metadata(message.from_user.id)
-    user_metadata = await db.get_metadata_code(message.from_user.id)
+    bool_metadata = find(int(message.chat.id))[2]
+    user_metadata = find(int(message.chat.id))[3]
     await ms.delete()
     if bool_metadata:
-
-        return await message.reply_text(f"Your Current Metadata:-\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(ON))
-
-    return await message.reply_text(f"Your Current Metadata:-\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(OFF))
+        return await message.reply_text(f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ",quote=True, reply_markup=InlineKeyboardMarkup(ON))
+    return await message.reply_text(f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ",quote=True, reply_markup=InlineKeyboardMarkup(OFF))
 
 
 @Client.on_callback_query(filters.regex('.*?(custom_metadata|metadata).*?'))
@@ -32,27 +31,27 @@ async def query_metadata(bot: Client, query: CallbackQuery):
 
     if data.startswith('metadata_'):
         _bool = data.split('_')[1]
-        user_metadata = await db.get_metadata_code(query.from_user.id)
+        user_metadata = find(int(query.message.chat.id))[3]
 
         if bool(eval(_bool)):
-            await db.set_metadata(query.from_user.id, bool_meta=False)
-            await query.message.edit(f"Your Current Metadata:-\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(OFF))
+            setmeta(int(query.message.chat.id), bool_meta=False)
+            await query.message.edit(f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(OFF))
 
         else:
-            await db.set_metadata(query.from_user.id, bool_meta=True)
-            await query.message.edit(f"Your Current Metadata:-\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(ON))
+            setmeta(int(query.message.chat.id), bool_meta=True)
+            await query.message.edit(f"**Your Current Metadata :-**\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(ON))
 
     elif data == 'cutom_metadata':
         await query.message.delete()
         try:
             try:
-                metadata = await bot.ask(text=Txt.SEND_METADATA, chat_id=query.from_user.id, filters=filters.text, timeout=30, disable_web_page_preview=True)
+                metadata = await bot.ask(text=script.METADATA_TXT, chat_id=query.from_user.id, filters=filters.text, timeout=30, disable_web_page_preview=True, reply_to_message_id=query.message.id)
             except ListenerTimeout:
-                await query.message.reply_text("⚠️ Error!!\n\n**Request timed out.**\nRestart by using /metadata", reply_to_message_id=query.message.id)
+                await query.message.reply_text("⚠️ Error !!\n\n**Request Timed Out.**\n\nRestart By Using /metadata", reply_to_message_id=query.message.id)
                 return
             print(metadata.text)
             ms = await query.message.reply_text("**Please Wait...**", reply_to_message_id=metadata.id)
-            await db.set_metadata_code(query.from_user.id, metadata_code=metadata.text)
-            await ms.edit("**Your Metadta Code Set Successfully ✅**")
+            setmetacode(int(query.message.chat.id), metadata_code=metadata.text)
+            await ms.edit("**Your Metadata Code Set Successfully ✅**")
         except Exception as e:
             print(e)
